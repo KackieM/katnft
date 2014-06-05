9f694f67138df70fa3544bd2182482fa8fdb9f22
Katherine Mattison <kbmedisto@gmail.com>
2014-06-05 16:07:17 -0400

    makin some progress and understanding more re my dev docs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,13 +5,50 @@ angular
     'ngCookies',
     'ngResource',
     'ngSanitize',
-    'ngRoute'
+    'ngRoute',
+    'firebase'
   ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/blog', {
+        templateUrl: 'views/blog-list.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/new', {
+        templateUrl: 'views/blog-create.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/blog/:id', {
+        templateUrl: 'views/blog-detail.html',
+        controller: 'PostCtrl'
+      })
+      .when('/blog/:id/edit', {
+        templateUrl: 'views/blog-editDetail.html',
+        controller: 'PostCtrl'
+      })
+      .when('/products', {
+        templateUrl: 'views/products.html',
+        controller: 'ProductsCtrl'
+      })
+      .when('/productsDetail', {
+        templateUrl: 'views/productsDetail.html',
+        controller: 'ProductsCtrl'
+      })
+      .when('/orderCreate', {
+        templateUrl: 'views/orderCreate.html',
+        controller: 'OrdersCtrl'
+      })
+      .when('/summaryOrder', {
+        templateUrl: 'views/orderSummary.html',
+        controller: 'SummaryCtrl'
+      })
+      .when('/orderSummaryEdit', {
+        templateUrl: 'views/orderSummaryEdit.html',
+        controller: 'OrdersCtrl'
       })
       .otherwise({
         redirectTo: '/'
